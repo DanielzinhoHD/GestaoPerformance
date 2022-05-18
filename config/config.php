be3d4eb97cbbbf209e93d915279b5a1981cafcd7
Danielzinho <christiandanielgomes@gmail.com>
2022-05-17 22:45:11 -0300

    added dir variables

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+#Arquivos diretórios raízes;
+
+$PastaInterna= '';
+define('DIRPAGE', "https://{$_SERVER['HTTP_HOST']}/{$PastaInterna}");
+
+if(substr($_SERVER['DOCUMENT_ROOT'], -1) == '/'){
+  define('DIRREQ', "https://{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}");
+}else{
+  define('DIRREQ', "https://{$_SERVER['DOCUMENT_ROOT']}/{$PastaInterna}");
+}
+
+#DIretórios Específicos;
+define('DIRIMG', DIRPAGE."public/img");
+define('DIRCSS', DIRPAGE."public/css");
+define('DIRJS', DIRPAGE."public/js");
+define('DIRADM', DIRPAGE."public/adm");
+
+#Acesso ao banco de dados;
+define('HOST', "localhost");
+define('DB', "sistema");
+define('USER', "root");
+define('PASS', "");
